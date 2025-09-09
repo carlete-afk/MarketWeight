f04333149a8cd33b61ef0c0d5c7b1f0bbe130bbc
carlete-afk <carlos.belloet12de1@gmail.com>
2025-09-09 11:06:57 -0300

    Tabla moneda se muestra en el Index del MVC

---
+++ src/MarketWeight.MVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MarketWeight.MVC.Models;
+using MarketWeight.Core;
+using MarketWeight.Ado.Dapper;
+using MarketWeight.Core.Persistencia;
+
+namespace MarketWeight.MVC.Controllers;
+
+public class HomeController : Controller
+{
+
+    private IRepoMoneda _repoMoneda;
+
+    public HomeController(IRepoMoneda repoMoneda)
+    {
+        _repoMoneda = repoMoneda;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var monedas = await _repoMoneda.ObtenerAsync();
+        return View(monedas);
+    }
+}
---
+++ src/MarketWeight.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MarketWeight.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
